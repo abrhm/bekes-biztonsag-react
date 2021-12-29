95b97176593c073a420ee99538ca8e19bef0b031
Gabor Abraham <godgab@gmail.com>
2021-12-29 14:55:28 +0100

    Add firebase config

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,7 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -15,3 +19,24 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDCOisHA0evB5eCU7MEP9IfYLFBxA23cac",
+  authDomain: "bekes-biztonsag.firebaseapp.com",
+  projectId: "bekes-biztonsag",
+  storageBucket: "bekes-biztonsag.appspot.com",
+  messagingSenderId: "134594532388",
+  appId: "1:134594532388:web:e3b852c4862259b49d74b1",
+  measurementId: "G-3VS11593HD"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
